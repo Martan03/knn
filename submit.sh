b88eb7d909049beea3d43c92677fbc776b5239a8
Martan03 <martinslezak03@gmail.com>
2026-05-09 19:08:34 +0200

    Update submit not to include pycache

--- submit.sh
+++ submit.sh
@@ -4,4 +4,5 @@ NAME=${1:-xsleza26}
 
 typst c proposal.typ
 mv proposal.pdf report.pdf
-zip -r $NAME.zip src main.py proposal.pdf README.md
+zip -r $NAME.zip src main.py proposal.pdf README.md \
+    -x "*/__pycache__" -x "*/__pycache__/*"
